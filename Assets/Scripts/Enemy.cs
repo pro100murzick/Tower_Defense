4386ee95bcec955a0916c4b931fd7dbdf8053297
unknown <mkiricenko289@gmail.com>
2021-05-14 17:46:44 +0300

    Add enemy spawner script and object, add enemy script.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetMoveSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+}
---
+++ Assets/Scripts/Enemy_Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawner : MonoBehaviour
+{
+
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 5f;
+    [SerializeField] Enemy[] enemyPrefabArray;
+    bool spawn = true;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnAttakers();
+        }
+    }
+
+    private void SpawnAttakers()
+    {
+        int enemyIndex = Random.Range(0, enemyPrefabArray.Length);
+        Spawn(enemyPrefabArray[enemyIndex]);
+    }
+
+    private void Spawn(Enemy enemy)
+    {
+        Enemy newEnemy = Instantiate(enemy, transform.position, Quaternion.identity);
+    }
+    public void StopSpawn()
+    {
+        spawn = false;
+    }
+}
