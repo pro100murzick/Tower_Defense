2160bdfda42bc67c10974d6fe6c89e467be4316d
unknown <mkiricenko289@gmail.com>
2021-05-19 18:28:23 +0300

    Add defender object and defender script, addc cactus defender animation. And projectile script to move zuchinni.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] int damage = 100;
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float rotationSpeed = 1f;
+
+    public int GetDamage() { return damage; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        // transform.Rotate(0, 0, rotationSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+    }
+}
