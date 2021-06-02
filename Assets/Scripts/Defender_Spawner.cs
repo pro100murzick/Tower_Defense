5d9baa874f2471f1df833bc2c3f618bbccb9f3dd
Кириченко Миша <mkiricenko289@gmail.com>
2021-06-02 15:44:15 +0300

    Fixed bugs and added Defender_Selector

---
+++ Assets/Scripts/Defender_Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender_Spawner : MonoBehaviour
+{
+
+    Defender defender;
+
+    public void SetSelectedDefender(Defender selectedDefender)
+    {
+        defender = selectedDefender;
+    }
+
+    private void OnMouseDown()
+    {
+        Vector2 worldPos = GetSquareClicked();
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Debug.Log("X = " + clickPos.x.ToString() + "Y = " + clickPos.y.ToString());
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Debug.Log("X = " + worldPos.x.ToString() + "Y = " + worldPos.y.ToString());
+        return new Vector2();
+    }
+
+}
