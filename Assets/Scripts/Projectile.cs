02e311a3e2861f943d0db62cbc32c7608d0b40f4
Кириченко Миша <mkiricenko289@gmail.com>
2021-06-02 14:50:52 +0300

    bugs fixed

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] int damage = 100;
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float rotationSpeed = 1f;
+
+    public int GetDamage() { return damage; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        // transform.Rotate(0, 0, rotationSpeed * Time.deltaTime, Space.Self);
+        //transform.Rotate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+    }
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var enemyHealth = otherCollider.GetComponent<Health>();
+        Enemy enemy = otherCollider.GetComponent<Enemy>();
+        if(enemyHealth && enemy)
+        {
+            enemyHealth.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
